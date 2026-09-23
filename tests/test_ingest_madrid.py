a35f1a2d16a745b80a63108bfd8f8b7bbc5c91fd
Rubén Rey Souto <rubenreysouto@gmail.com>
2026-09-23 23:26:24 +0200

    test: cover Madrid GeoJSON boundaries

--- tests/test_ingest_madrid.py
+++ tests/test_ingest_madrid.py
@@ -2,13 +2,11 @@ import unittest
 
 from scripts.ingest_madrid import (
     build_area_index,
-    decode_topology_arc,
-    geometry_polygons,
+    geojson_geometry_polygons,
     match_incident_area,
     metric_slug,
     normalize_name,
     parse_resource_month,
-    stitch_ring,
 )
 
 
@@ -40,40 +38,41 @@ class MadridSourceTests(unittest.TestCase):
         )
 
 
-class MadridTopologyTests(unittest.TestCase):
-    def setUp(self):
-        self.topology = {
-            "type": "Topology",
-            "transform": {
-                "scale": [0.1, 0.1],
-                "translate": [-4.0, 40.0],
-            },
-            "arcs": [
-                [[0, 0], [10, 0], [0, 10]],
-                [[10, 10], [-10, 0], [0, -10]],
-            ],
-        }
-
-    def test_decode_arc_applies_delta_and_transform(self):
-        decoded = decode_topology_arc(
-            [[0, 0], [10, 0], [0, 10]],
-            [0.1, 0.1],
-            [-4.0, 40.0],
-        )
-        self.assertEqual(decoded, [(-4.0, 40.0), (-3.0, 40.0), (-3.0, 41.0)])
-
-    def test_negative_arc_reverses_source_arc(self):
-        ring = stitch_ring(self.topology, [0, -2])
-        self.assertEqual(ring[0], (-4.0, 40.0))
-        self.assertEqual(ring[-1], ring[0])
-        self.assertGreaterEqual(len(ring), 4)
-
+class MadridGeoJsonTests(unittest.TestCase):
     def test_polygon_geometry_is_converted_to_shared_shape(self):
-        geometry = {"type": "Polygon", "arcs": [[0, -2]], "properties": {"COD_BAR": "011"}}
-        polygons = geometry_polygons(self.topology, geometry)
+        geometry = {
+            "type": "Polygon",
+            "coordinates": [[
+                [-3.71, 40.40],
+                [-3.70, 40.40],
+                [-3.70, 40.41],
+                [-3.71, 40.40],
+            ]],
+        }
+        polygons = geojson_geometry_polygons(geometry)
         self.assertEqual(len(polygons), 1)
         self.assertEqual(polygons[0]["holes"], [])
         self.assertEqual(polygons[0]["outer"][0], polygons[0]["outer"][-1])
+
+    def test_multipolygon_geometry_is_supported(self):
+        ring = [
+            [-3.71, 40.40],
+            [-3.70, 40.40],
+            [-3.70, 40.41],
+            [-3.71, 40.40],
+        ]
+        polygons = geojson_geometry_polygons({
+            "type": "MultiPolygon",
+            "coordinates": [[ring], [ring]],
+        })
+        self.assertEqual(len(polygons), 2)
+
+    def test_implausible_coordinates_fail_closed(self):
+        with self.assertRaises(RuntimeError):
+            geojson_geometry_polygons({
+                "type": "Polygon",
+                "coordinates": [[[0, 0], [1, 0], [1, 1], [0, 0]]],
+            })
 
 
 if __name__ == "__main__":
