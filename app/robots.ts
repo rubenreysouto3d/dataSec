e2a4a4a9a86afb789d23927949df4cf1e69e0ffd
Rubén Rey Souto <rubenreysouto@gmail.com>
2026-09-24 09:05:08 +0200

    fix: statically export robots policy

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 function siteUrl() {
   return (process.env.NEXT_PUBLIC_SITE_URL ?? "https://rubenreysouto3d.github.io/dataSec").replace(/\/$/, "");
 }
